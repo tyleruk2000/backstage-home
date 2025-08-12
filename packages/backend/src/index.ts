3f312e39f708568b139554b951a897fd2e50dcb8
Tyler Allen <tyleruk2000@gmail.com>
2025-08-12 15:32:56 +0100

    Knows about itself

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -24,6 +24,7 @@ backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
 
 // catalog plugin
 backend.add(import('@backstage/plugin-catalog-backend'));
+backend.add(import('@backstage/plugin-catalog-backend-module-github'));
 backend.add(
   import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
 );
